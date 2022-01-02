ffc7ad042325e8c1689fbed2088c0929f2889616
Boris Pedraza <bpedrazaa@gmail.com>
2022-01-02 18:23:29 -0400

    Code to play music inside SPA aggregated

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,27 @@
+// import React, { useState, useEffect } from "react";
+
+// const useAudio = (url) => {
+//   const [audio] = useState(new Audio(url));
+//   const [playing, setPlaying] = useState(false);
+
+//   const toggle = () => setPlaying(!playing);
+
+//   useEffect(() => {
+//     playing ? audio.play() : audio.pause();
+//   }, [playing]);
+
+//   useEffect(() => {
+//     audio.addEventListener("ended", () => setPlaying(false));
+//     return () => {
+//       audio.removeEventListener("ended", () => setPlaying(false));
+//     };
+//   }, []);
+
+//   return [playing, toggle];
+// };
+
 const Card = ({ songInfo }) => {
+  //const [playing, toggle] = useAudio(songInfo.previewUrl);
   // To render
   return (
     <div className="card m-2 mb-3" style={{ width: 10 + "rem" }}>
@@ -19,6 +42,10 @@ const Card = ({ songInfo }) => {
         <li className="list-group-item">PRICE: {songInfo.price} $</li>
       </ul>
       <div className="card-body">
+        {/* <button className="btn btn-outline-dark" onClick={toggle}>
+          {playing ? "Pause" : "Play Preview"}
+        </button> */}
+
         <a
           href={songInfo.previewUrl}
           target="_blank"

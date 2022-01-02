5095ed04d0cc6c47c57b749a637fcd9c09df67e9
Boris Pedraza <bpedrazaa@gmail.com>
2022-01-02 19:05:25 -0400

    Pagination code implemented (Review needed)

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,6 +1,29 @@
+//import ReactPaginate from "react-paginate";
+//import { useState, useEffect } from "react";
 import Card from "./Card";
 
 const Cards = ({ songs }) => {
+  // const [offset, setOffset] = useState(0);
+  // const [data, setData] = useState([]);
+  // const [perPage] = useState(10);
+  // const [pageCount, setPageCount] = useState(0);
+
+  // const manipulateData = (songs) => {
+  //   setData(songs);
+  //   setPageCount(Math.ceil(data.length / perPage));
+
+  //   console.log(songs);
+  // };
+
+  // useEffect(() => {
+  //   manipulateData(songs);
+  // }, [offset]);
+
+  // const handlePageClick = (e) => {
+  //   const selectedPage = e.selected;
+  //   setOffset(selectedPage + 1);
+  // };
+
   // To render
   return (
     //Put cards in a column (side by side)
@@ -14,6 +37,20 @@ const Cards = ({ songs }) => {
           <Card songInfo={song} key={song.trackId} />
         </div>
       ))}
+
+      {/* <ReactPaginate
+        previousLabel={"prev"}
+        nextLabel={"next"}
+        breakLabel={"..."}
+        breakClassName={"break-me"}
+        pageCount={pageCount}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={5}
+        onPageChange={handlePageClick}
+        containerClassName={"pagination"}
+        subContainerClassName={"pages pagination"}
+        activeClassName={"active"}
+      /> */}
     </div>
   );
 };

809c0f213b2e1c060f84a411d37cf010ba3ac033
Boris Pedraza <bpedrazaa@gmail.com>
2022-01-02 20:29:54 -0400

    Table component updated

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,10 +2,10 @@ const Table = ({ songs }) => {
   // To render
   return (
     <div>
-      <div class="alert alert-info m-1" role="alert">
+      <div class="alert alert-info m-3" role="alert">
         TABLE OF SONGS
       </div>
-      <table className="table m-2">
+      <table className="table m-1">
         <thead className="thead-dark">
           <tr>
             <th scope="col">Song Name</th>
@@ -13,6 +13,7 @@ const Table = ({ songs }) => {
             <th scope="col">Album Name</th>
             <th scope="col">Time (Seconds)</th>
             <th scope="col">Price ($)</th>
+            <th scope="col">Preview Link</th>
           </tr>
         </thead>
         <tbody>
@@ -23,6 +24,15 @@ const Table = ({ songs }) => {
               <td>{songInfo.albumName}</td>
               <td>{songInfo.time}</td>
               <td>{songInfo.price}</td>
+              <td>
+                <a
+                  href={songInfo.previewUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <div>Link to Preview</div>
+                </a>
+              </td>
             </tr>
           ))}
         </tbody>
